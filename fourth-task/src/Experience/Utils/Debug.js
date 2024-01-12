f884f01c17d6ff0d8bc0884040f82fcb7bb45328
h0wter <91832203+h0wter@users.noreply.github.com>
2024-01-12 10:34:24 +0200

    update formatting

--- fourth-task/src/Experience/Utils/Debug.js
+++ fourth-task/src/Experience/Utils/Debug.js
@@ -1,14 +1,11 @@
-import GUI from 'lil-gui'
+import GUI from "lil-gui";
 
-export default class Debug
-{
-    constructor()
-    {
-        this.active = window.location.hash === '#debug'
+export default class Debug {
+  constructor() {
+    this.active = window.location.hash === "#debug";
 
-        if(this.active)
-        {
-            this.ui = new GUI()
-        }
+    if (this.active) {
+      this.ui = new GUI();
     }
+  }
 }
